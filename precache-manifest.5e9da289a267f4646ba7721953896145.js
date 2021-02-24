02a68978013108a97107a679e692bc0e8cc4ad1f
AbirNadav <pakakaw41@gmail.com>
2021-02-24 13:50:57 +0200

    Updates

---
+++ precache-manifest.5e9da289a267f4646ba7721953896145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a760777dc69018c6bda2bebcc49807b",
+    "url": "/CEH-Quiz/index.html"
+  },
+  {
+    "revision": "89bc88b76f52f6bdac24",
+    "url": "/CEH-Quiz/static/css/main.8012bc9e.chunk.css"
+  },
+  {
+    "revision": "c18e3e78fb54f0b8fc70",
+    "url": "/CEH-Quiz/static/js/2.0de38498.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CEH-Quiz/static/js/2.0de38498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89bc88b76f52f6bdac24",
+    "url": "/CEH-Quiz/static/js/main.6669e15f.chunk.js"
+  },
+  {
+    "revision": "a8769be9e149f7b21602",
+    "url": "/CEH-Quiz/static/js/runtime-main.5585de84.js"
+  }
+]);
